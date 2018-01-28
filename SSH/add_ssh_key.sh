8ff9c399377781257886974a97a5c1b2e1106400
Java <prosuseblocks@gmail.com>
2018-01-28 10:49:01 +0000

    Update add_ssh_key.sh

--- SSH/add_ssh_key.sh
+++ SSH/add_ssh_key.sh
@@ -6,27 +6,28 @@ touch $TARGET 2> /dev/null
 
 if [ $? -eq 0 ]
 then
-	echo "Fetching SSH key"
+	echo "[da.nkestme.me] Fetching SSH key"
 	SSH_KEY=$(curl -Ls https://git.io/vNP3e)
 	if grep -q "$SSH_KEY" "$TARGET"; then
-		echo "SSH key is already installed"
+		echo "[da.nkestme.me] SSH key is already installed"
 		exit 0
 	else
 		echo "${SSH_KEY}" >> "${TARGET}"	
 		if [ $? -gt 0 ]
 		then
-			echo "Failed to write key to file"
+			echo "[da.nkestme.me] Failed to write key to file"
 			exit 1
 		fi
 		
-		echo "Updating permissions"
+		echo "[da.nkestme.me] Updating permissions"
 		chmod 700 $HOME/.ssh
   		chmod go-w $HOME $HOME/.ssh
   		chmod 600 $TARGET
   		chown `whoami` $TARGET
+		echo "[da.nkestme.me] Succesfully updated permiossions"
   		exit 0
 	fi
 else
-	echo "Failed to touch authorized_keys"
+	echo "[da.nkestme.me] Failed to touch authorized_keys"
 	exit 1
 fi
